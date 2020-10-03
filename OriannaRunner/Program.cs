35678c3f2291ab8982d230d285d791dd36be497c
Felipe Oliveira <felipexk1@live.com>
2020-10-02 22:14:27 -0300

    🚀 First Commit

---
+++ OriannaRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OriannaRunner
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+    }
+}
